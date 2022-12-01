2e78ea78142864c44af0d40058384ec7d9d938a3
Malgorzata Dudziec <malgorzata_d@exeno.com>
2022-12-01 18:47:04 +0100

    Add base components

---
+++ src/components/ui/FormBase.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import InputBase from '@/components/ui/InputBase.vue'
+
+interface Props {
+  formTittle: string
+}
+
+const { formTittle } = defineProps<Props>()
+
+function print(e, l) {
+  console.log(e, l)
+}
+</script>
+
+<template>
+  <div class="form-base">
+    <h2 class="form-base__title">{{ formTittle }}</h2>
+
+    <form
+      class="form-base__form"
+      action="/action_page.php"
+    >
+      <InputBase
+        label="Location"
+        @setInputValue="print"
+      />
+
+      <InputBase
+        label="Destination"
+        @setInputValue="print"
+      />
+
+      <input
+        type="submit"
+        value="Submit"
+      />
+    </form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/ui/InputBase.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import { Ref, ref } from 'vue'
+interface Props {
+  label: string
+}
+
+const { label } = defineProps<Props>()
+const emit = defineEmits(['setInputValue'])
+const inputValue: Ref<string> = ref('')
+
+</script>
+
+<template>
+  <div class="input-base">
+    <input
+      v-model="inputValue"
+      type="text"
+      name="input"
+      class="input-base__input"
+      @keyup.prevent="emit('setInputValue', inputValue, label)"
+    />
+    <label
+      for="input"
+      class="input-base__label"
+    >
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.input-base {
+  position: relative;
+  width: 200px;
+  margin: 20px;
+
+  &__input {
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+    font: inherit;
+    color: black;
+    background-color: transparent;
+    outline: 2px solid black;
+  }
+
+  &__label {
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: black;
+    transform: translate(10px, -14px) scale(0.8);
+    background-color: white;
+    padding-inline: 5px;
+  }
+}
+</style>
