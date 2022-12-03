41f64603a6697dcfe3d60c8b56105eed0c73c26c
Malgorzata Dudziec <malgorzata_d@exeno.com>
2022-12-03 12:54:02 +0100

    Render inputs dynamically

--- src/components/ui/FormBase.vue
+++ src/components/ui/FormBase.vue
@@ -3,8 +3,8 @@ import { Ref, ref } from 'vue'
 import { useContext } from '@/composables/context'
 import InputBase from '@/components/ui/InputBase.vue'
 import LocationParams from '@/types/LocationParams'
-
-import InputLabels from '@/types/InputName'
+import FormInput from '@/types/FormInput'
+import InputLabels from '@/types/InputLabels'
 
 interface Props {
   formTittle: string
@@ -35,62 +35,64 @@ function submit() {
     destinations: destination.value,
     key: 'yvneAg8ShdBQhx722CiXNkyITufRQ',
   }
-  ctx.serviceApi.get(params)
+  ctx.serviceApi
+    .get(params)
     .then((data: any) => {
       distance.value = data.data.rows[0].elements[0].distance.text
     })
     .catch((e: any) => console.log(e))
 }
+
+const inputs: FormInput[] = [
+  { label: InputLabels.LOCATION, placeholder: '51.4822656,-0.1933769' },
+  { label: InputLabels.DESTINATION, placeholder: '51.4994794,-0.1269979' },
+]
 </script>
 
 <template>
   <div class="form-base">
     <h2 class="form-base__title">{{ formTittle }}</h2>
-    <p>This app calculates for you distance between two locations. Fill below inputs with the longitude and latitude in below form and click submit button to see the results.</p>
+    <p>
+      This app calculates for you distance between two locations. Fill below
+      inputs with the longitude and latitude in below form and click submit
+      button to see the results.
+    </p>
 
     <form class="form-base__form">
       <InputBase
-        :label="InputLabels.LOCATION"
-        placeholder="51.4822656,-0.1933769"
-        @saveInputValue="saveInputValue"
-      />
-
-      <InputBase
-        :label="InputLabels.DESTINATION"
-        placeholder="51.4994794,-0.1269979"
+        v-for="(input, index) in inputs"
+        :key="index"
+        :label="input.label"
+        :placeholder="input.placeholder"
         @saveInputValue="saveInputValue"
       />
     </form>
 
-          <button
-        @click="submit"
-      >
-        Submit
-      </button>
+    <button @click="submit">Submit</button>
 
-      {{ distance }}
+    {{ distance }}
   </div>
 </template>
 
 <style lang="scss" scoped>
 .form-base {
   width: 800px;
-      display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    align-content: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
 }
 
 .form-base__form {
   display: flex;
-    flex-direction: row;
+  flex-direction: row;
 }
 h2 {
   font-weight: 600;
   font-size: 2.4em;
 }
-  p{
-    font-size: 14px;
-  }
+p {
+  font-size: 14px;
+}
 </style>
---
+++ src/types/FormInput.ts
@@ -0,0 +1,8 @@
+import InputLabels from '@/types/InputLabels'
+
+interface FormInput {
+  label: InputLabels
+  placeholder: string
+}
+
+export default FormInput
